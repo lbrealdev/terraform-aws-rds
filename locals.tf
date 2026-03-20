af3961db330b0bea82c73efb20eed6c45f2aed42
lbrealdev <lbrealdeveloper@gmail.com>
2026-03-20 01:56:04 +0100

    feat: uncomment the rds_settings local map variable in v17

--- locals.tf
+++ locals.tf
@@ -39,21 +39,21 @@ locals {
     }
 
     # Example with custom name
-    # "v17" = {
-    #   name = "infra"
-    #
-    #   parameter_group = {
-    #     family      = "sqlserver-web-16.0"
-    #     description = "Infra adjustment parameter group"
-    #     parameters  = local.rds_parameters
-    #   }
-    #
-    #   option_group = {
-    #     engine_name          = "sqlserver-web"
-    #     major_engine_version = "16.00"
-    #     description          = "Infra adjustment option group"
-    #     options              = local.rds_options
-    #   }
-    # }
+    "v17" = {
+      name = "${var.prefix_name}-infra"
+
+      parameter_group = {
+        family      = "sqlserver-web-16.0"
+        description = "Infra adjustment parameter group"
+        parameters  = local.rds_parameters
+      }
+
+      option_group = {
+        engine_name          = "sqlserver-web"
+        major_engine_version = "16.00"
+        description          = "Infra adjustment option group"
+        options              = local.rds_options
+      }
+    }
   }
 }
